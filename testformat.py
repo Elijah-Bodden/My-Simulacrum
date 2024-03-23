2885d89d345fb1c59e1dbb1c099fc768509a2282
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2024-03-22 21:15:44 -0500

    Create testformat.py

---
+++ testformat.py
@@ -0,0 +1,16 @@
+from transformers import AutoTokenizer
+from datasets import load_from_disk
+import yaml
+from os import listdir
+import pandas as pd
+
+directory = listdir("http://workspace/axolotl/last_run_prepared/")[0]
+with open('config.yml', 'r') as f:
+    cfg = yaml.safe_load(f)
+model_id = cfg['base_model']
+tok = AutoTokenizer.from_pretrained(model_id)
+ds = load_from_disk(f'last_run_prepared/{directory}/')
+row = ds[0]
+print(tok.decode(row['input_ids']))
+print(pd.DataFrame([{'token': tok.decode(i), 'label': l, 'id':i} for i,l in
+              zip(row['input_ids'], row['labels'])]))
