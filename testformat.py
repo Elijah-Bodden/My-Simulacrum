7a58147593ed33ad1a1b8d620f613f5c38f1a998
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2024-03-22 21:30:55 -0500

    Update testformat.py

--- testformat.py
+++ testformat.py
@@ -13,4 +13,4 @@ ds = load_from_disk(f'last_run_prepared/{directory}/')
 row = ds[0]
 print(tok.decode(row['input_ids']))
 print(pd.DataFrame([{'token': tok.decode(i), 'label': l, 'id':i} for i,l in
-              zip(row['input_ids'], row['labels'])]))
+              zip(row['input_ids'], row['labels'])]).to_string())
