629a30bece45c4988521826d8ba1ddae9f777e9b
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2024-03-23 20:26:33 -0500

    Update run.bash

--- run.bash
+++ run.bash
@@ -3,6 +3,6 @@ rm -rf /workspace/axolotl
 git clone https://github.com/OpenAccess-AI-Collective/axolotl.git
 cd axolotl
 pip install --no-deps -e .
-export WANDB_API_KEY=498acea71d841dd41fdd56df472f2c4c6131fafc
-wget https://raw.githubusercontent.com/Elijah-Bodden/Mind-upload/main/config.yml
+export WANDB_API_KEY=$1
+wget https://raw.githubusercontent.com/$2/Mind-upload/main/config.yml
 CUDA_VISIBLE_DEVICES="" python -m axolotl.cli.preprocess config.yml
