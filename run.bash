4e564fe5a383321f177ca7ffbd63933501ed25c4
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2024-03-23 21:40:42 -0500

    Update run.bash

--- run.bash
+++ run.bash
@@ -3,6 +3,5 @@ rm -rf /workspace/axolotl
 git clone https://github.com/OpenAccess-AI-Collective/axolotl.git
 cd axolotl
 pip install --no-deps -e .
-export WANDB_API_KEY=$1
-wget https://raw.githubusercontent.com/$2/My-Simulacrum/main/config.yml
+wget https://raw.githubusercontent.com/$1/My-Simulacrum/main/config.yml
 CUDA_VISIBLE_DEVICES="" python -m axolotl.cli.preprocess config.yml
