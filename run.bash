ee52324953f71127398e3534e6cc598a2b736d77
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2024-03-23 19:22:25 -0500

    Update run.bash

--- run.bash
+++ run.bash
@@ -6,4 +6,3 @@ pip install --no-deps -e .
 export WANDB_API_KEY=498acea71d841dd41fdd56df472f2c4c6131fafc
 wget https://raw.githubusercontent.com/Elijah-Bodden/Mind-upload/main/config.yml
 CUDA_VISIBLE_DEVICES="" python -m axolotl.cli.preprocess config.yml
-accelerate launch -m axolotl.cli.train config.yml
